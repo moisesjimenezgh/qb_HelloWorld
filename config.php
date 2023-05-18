9096b2c23305e042d19c31406ea30e37f768a6dd
moisesjimenezgh <124845781+moisesjimenezgh@users.noreply.github.com>
2023-05-17 17:48:44 -0700

    Update config.php
    Removed key

--- config.php
+++ config.php
@@ -13,8 +13,8 @@ Realm ID: 9130356631056256
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'ABqM3JVyXLNcqOOWJVxwZkQC2UhreBmpiV4rw12rjxR0X5rDxI',
-    'client_secret' => 'dcYlA3fXsNSlvCJgQhJOrB83VabTuLz4cLhL6IBU',
+    'client_id' => '',
+    'client_secret' => '',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'https://sugarstage.solimarsystems.com/solimar/qb/qb_HelloWorld/callback.php',
 )
